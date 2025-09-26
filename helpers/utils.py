578cdb44d2d6609dc150f6d3f87bdd6b3ab0da5a
Dan Nelson <44625150+danelson813@users.noreply.github.com>
2025-09-26 16:30:46 -0700

    added change  to helpers/utilities

--- helpers/utils.py
+++ helpers/utils.py
@@ -5,10 +5,10 @@ from loguru import logger
 
 def make_soup(url: str) -> bs:
     try:
-        page = requests.get(url)
+        page_ = requests.get(url)
     except FileNotFoundError:
         logger.info("trouble loading the url")
-    soup_ = bs(page.text, "html.parser")
+    soup_ = bs(page_.text, "html.parser")
     return soup_
 
 
@@ -20,8 +20,7 @@ def parse_page(hats_: list, results_: list) -> list:
                 "span", class_="price-item price-item--sale price-item--last"
             ).text.strip(),
             "manufacturer": hat.find(
-                "div", class_="caption-with-letter-spacing light"
-            ).text,
+                "div", class_="caption-with-letter-spacing light").text,
         }
         results_.append(result)
     return results_
